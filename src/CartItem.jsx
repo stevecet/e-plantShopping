1b686154ed0629411419553295c37d02f5891ac8
stevecet <steveceto@outlook.fr>
2025-06-18 12:53:05 +0100

    candidate dashboard

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -69,7 +69,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}><a href="#"> Continue Shopping </a></button>
+        <button className="get-started-button"><a href="/e-plantShopping"> Continue Shopping </a></button>
         <br />
         <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
